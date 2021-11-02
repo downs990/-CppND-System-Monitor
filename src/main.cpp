958db82d05883007a0ceb371e0d74391bc31c8be
downs990 <downscourtney17@gmail.com>
2021-11-02 09:23:03 +0000

    added comments for running code

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include "ncurses_display.h"
 #include "system.h"
 
+
+// How to run the program  ??
+
+//1,    make build 
+//2,    ./build/monitor 
+
 int main() {
   System system;
   NCursesDisplay::Display(system);
